73abe54dc2a221e9ed4b4a603110471ca3ab9d4d
Ali AlSaghir <alisalsaghir@gmail.com>
2024-07-22 10:16:28 +0300

    initial commit

---
+++ READme.md
@@ -0,0 +1,5 @@
+# Guess My Number
+
+### A very simple guessing game
+
+Guess the number between 1 and 100
---
+++ script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+let nb = Math.trunc(Math.random() * 100 + 1);
+let score = 20;
+let highscore = 0;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Not a number';
+  } else if (guess == nb) {
+    document.querySelector('.message').textContent = 'You got it!';
+    document.querySelector('.number').textContent = nb;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = score;
+    }
+  } else if (guess !== nb) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > nb ? 'Too high!' : 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  nb = Math.trunc(Math.random() * 100 + 1);
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  score = 20;
+});
